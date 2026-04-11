b9b96c3b2a68d3be90a2529fba3b0f3f1b233b3b
Qhamani Magasana <qhamanimagasana@gmail.com>
2026-04-12 01:12:23 +0200

    got axios, api, created two functions
    created functions for current weather and for it to fetch the data and change the html, created function for the city and for it to also get the data from the same api and change the html too. deleted the h1 contents and current weather on html then put the on js.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+function changeWeather(response){
+    let currentWeather = document.querySelector("#current-temperature");
+    let temperature = response.data.temperature.current;
+    currentWeather.innerHTML = Math.round(temperature);
+
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    
+}
+function searchCity(city){
+  
+    let apiKey = "49a8a63t5f3e45e0f637cbabo60c59fa";
+      let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+   axios.get(apiUrl).then(changeWeather);
+}
+
 function searchFormSubmit(event){
     event.preventDefault();
     let searchEngen = document.querySelector("#search-engen");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchEngen.value;
-
+    
+   searchCity(searchEngen.value);
 }
 
 
 let searchForm=document.querySelector("#form-element");
 searchForm.addEventListener("submit", searchFormSubmit);
 
+searchCity("Johannesburg");
