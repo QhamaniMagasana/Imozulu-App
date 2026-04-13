55e9d332aa47ea92d25d8956d5cc34f809bf5b9f
Qhamani Magasana <qhamanimagasana@gmail.com>
2026-04-13 14:13:16 +0200

    worked on js to get all the elements from the api.

--- index.js
+++ index.js
@@ -2,16 +2,58 @@ function changeWeather(response){
     let currentWeather = document.querySelector("#current-temperature");
     let temperature = response.data.temperature.current;
     currentWeather.innerHTML = Math.round(temperature);
+    console.log(response.data);
 
+    let weatherCondition = document.querySelector("#weather-description");
+    weatherCondition.innerHTML = response.data.condition.description;
+
+    let temperatureHumidity =  document.querySelector("#humidity");
+    let humidity = response.data.temperature.humidity;
+    temperatureHumidity.innerHTML = `${humidity}%`;
+
+    let windSpeed = document.querySelector("#wind-speed");
+    let speedWind = response.data.wind.speed;
+    windSpeed.innerHTML = `${speedWind} m/s`;
 
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
+
+    let dayTime = document.querySelector("#day-time");
+    let date = new Date(response.data.time * 1000);
+    dayTime.innerHTML = currentDate(date);
+
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather.icon"/>`;
+
     
 }
+
+function currentDate(date){
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = days[date.getDay()];
+
+    if(minutes <10){
+        minutes = `0${minutes}`;
+    }
+
+    if(hours <10){
+        hours = `0${hours}`;
+    }
+   
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function searchCity(city){
-  
-    let apiKey = "49a8a63t5f3e45e0f637cbabo60c59fa";
-      let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+     let apiKey = "49a8a63t5f3e45e0f637cbabo60c59fa";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
    axios.get(apiUrl).then(changeWeather);
 }
 
@@ -27,3 +69,7 @@ let searchForm=document.querySelector("#form-element");
 searchForm.addEventListener("submit", searchFormSubmit);
 
 searchCity("Johannesburg");
+
+
+
+
